2a9c3f27f32556947cf230fc7efa09f029c8102b
Andrea Tilcerová <andrea.tilcer@gmail.com>
2022-05-21 10:26:58 +0200

    Routování

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { Reservation } from '../Reservation';
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/reservation" element={<Reservation />} />
+    </Routes>
     <Footer />
+    <Outlet />
   </>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 import './style.css';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 
 export const API_BASE_URL = 'http://leviexpress-backend.herokuapp.com/api';
 
-createRoot(document.querySelector('#app')).render(<App />, );
+createRoot(document.querySelector('#app')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
